2b4637b259cb151a699927a302d276d58bc98a80
t00199360 <43175537+t00199360@users.noreply.github.com>
2019-09-18 08:57:19 +0100

    added the update scale
    Read comments for steps to finish the method

--- TransformationsLiam.cs
+++ TransformationsLiam.cs
@@ -31,6 +31,31 @@ public class Transformations : MonoBehaviour {
         
 	}
 
+    private void matrixScale()
+    {
+        Vector3[] cube = new Vector3[8];
+        cube[0] = new Vector3(1, 1, 1);
+        cube[1] = new Vector3(-1, 1, 1);            //change the cube to the previous results
+        cube[2] = new Vector3(-1, -1, 1);
+        cube[3] = new Vector3(1, -1, 1);
+        cube[4] = new Vector3(1, 1, -1);
+        cube[5] = new Vector3(-1, 1, -1);
+        cube[6] = new Vector3(-1, -1, -1);
+        cube[7] = new Vector3(1, -1, -1);
+
+        Vector3 startingAxis = new Vector3(14, 2, 2);
+        startingAxis.Normalize();                                       //change names from  rotation to scale matrix
+        Quaternion rotation = Quaternion.AngleAxis(10, startingAxis);
+        Matrix4x4 rotationMatrix =
+            Matrix4x4.TRS(new Vector3(0, 0, 0),
+                            Quaternion.identity, new Vector3(0,0,0));
+        printMatrix(rotationMatrix);
+
+        Vector3[] ImageAfterRotation =
+            MatrixTransform(cube, rotationMatrix);
+        printVerts(ImageAfterRotation);
+    }
+
     private void printVerts(Vector3[] newImage)
     {
         for (int i = 0; i < newImage.Length; i++)
